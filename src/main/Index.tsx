ecf88869f5e41af73e2dd8d4e025911c21b80f5a
stpark89 <stpark89@naver.com>
2020-09-18 15:23:04 +0900

    React Router 추가

---
+++ src/main/Index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const Index: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>메인페이지</h1>
+    </div>
+  );
+};
+
+export default Index;
